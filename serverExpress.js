3dc4d6444a0b1244dc7a052f1ddfeb99f1a00af8
m1ndx <luisfer.isc@gmail.com>
2019-04-29 11:20:20 -0500

    cambiar el package.json

--- serverExpress.js
+++ serverExpress.js
@@ -1,17 +1,38 @@
 'use strict'
 const express = require('express');
 const fs = require('fs');
+const chalk = require('chalk');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
 let alumnos = JSON.parse(fs.readFileSync('alumnos.json'));
 
-app.use(express.static(__dirname + '/public'));
+let jsonParser = bodyParser.json();
+let corsOptions = {
+    origin:'http://127.0.0.1:5500',
+    optionsSuccessStatus: 200
+}
 
+
+app.use(jsonParser);
+app.use(cors(corsOptions));
+
+app.use(express.static(__dirname + '/public'));
 //app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-app.route('/home').get((req, res) => res.send('DASWorld HOME1'));
-app.route('/alumnos').get((req,res)=>res.json(alumnos));
-app.route('/alumnos/:id').get((req,res)=>{
+app.route('/api/alumno')
+    .get((req,res)=>res.json(alumnos))
+    .post((req,res)=>{
+        console.log(chalk.blue(JSON.stringify(req.body)));
+        console.log(chalk.blue(req.body.id));
+        alumnos.push(req.body);
+        console.log(alumnos);
+        res.status(201).send();
+    });
+
+app.route('/alumno/:id').get((req,res)=>{
     let id = req.params.id;
     let al = alumnos.find(alu=> alu.id==id);
     if(al){
